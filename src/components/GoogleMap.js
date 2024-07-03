ef7c37205df7660120e7a3a146bc482d92bf202a
RutiKraviz <r0548494906@gmail.com>
2024-07-03 21:14:21 +0300

    changeGoogleMaps

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
+import React, {useState, useEffect} from 'react';
+import {GoogleMap, DirectionsRenderer} from '@react-google-maps/api';
 
 const containerStyle = {
   width: '800px',
-  height: '600px'
+  height: '600px',
 };
 
 const center = {
-  lat: 41.648823, 
-  lng: -0.889085 
+  lat: 41.648823,
+  lng: -0.889085,
 };
 
 const GoogleMaps = () => {
@@ -18,47 +18,41 @@ const GoogleMaps = () => {
   const fetchRoute = () => {
     const directionsService = new window.google.maps.DirectionsService();
 
-    directionsService.route({
-      origin: 'רבי עקיבא 18,בני ברק, ישראל',
-      destination: ' רבן יוחנן בן זכאי 52, בני ברק, ישראל',
-      waypoints: [
-        { location: 'הרב קוק 16, בני ברק, ישראל', stopover: true },
-        { location: ' ירושלים 53, בני ברק, ישראל', stopover: true }
-      ],
-      travelMode: 'DRIVING'
-    }, (result, status) => {
-      if (status === window.google.maps.DirectionsStatus.OK) {
-        setResponse(result);
-        if (result.routes && result.routes.length > 0) {
-          const route = result.routes[0];
-          const newDurations = route.legs.map(leg => leg.duration.text);
-          setDurations(newDurations);
+    directionsService.route(
+      {
+        origin: 'רבי עקיבא 18,בני ברק, ישראל',
+        destination: ' רבן יוחנן בן זכאי 52, בני ברק, ישראל',
+        waypoints: [
+          {location: 'הרב קוק 16, בני ברק, ישראל', stopover: true},
+          {location: ' ירושלים 53, בני ברק, ישראל', stopover: true},
+        ],
+        travelMode: 'DRIVING',
+      },
+      (result, status) => {
+        if (status === window.google.maps.DirectionsStatus.OK) {
+          setResponse(result);
+          if (result.routes && result.routes.length > 0) {
+            const route = result.routes[0];
+            const newDurations = route.legs.map((leg) => leg.duration.text);
+            setDurations(newDurations);
+          }
+        } else {
+          console.error(`Directions service failed due to ${status}`);
         }
-      } else {
-        console.error(`Directions service failed due to ${status}`);
       }
-    })
-  }
-  // console.count("Out");
+    );
+  };
 
   useEffect(() => {
     fetchRoute();
   }, []);
 
   return (
-      <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={7}
-    >
-        {response && (
-        <DirectionsRenderer
-          options={{ directions: response }}
-        />
-      )}
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={7}>
+      {response && <DirectionsRenderer options={{directions: response}} />}
 
       {durations.length > 0 && (
-        <div style={{ position: 'absolute', top: 10, left: 10, background: 'white', padding: 10, zIndex: 1000 }}>
+        <div style={{position: 'absolute', top: 10, left: 10, background: 'white', padding: 10, zIndex: 1000}}>
           <h3>Durations Between Points:</h3>
           <ul>
             {durations.map((duration, index) => (
@@ -72,50 +66,3 @@ const GoogleMaps = () => {
 };
 
 export default GoogleMaps;
-
-
-
-// import React from 'react';
-// import GoogleMapReact from 'google-map-react';
-
-// const arr=[{lat:32.1,lng:34.84,text:'כינוי'}]
-
-// const AnyReactComponent = ({ text }) => <h1> {text}</h1>;
-
-// export default function GoogleMap() {
-//   const defaultProps = {
-//     center: {
-//       lat:32.089731,
-//       lng:34.832381
-//     },
-//     zoom: 16 // Zoom level
-//   };
-
-//   return (
-//     <div style={{ height: '400px', width: '50%'}}>
-//       <GoogleMapReact
-//         bootstrapURLKeys={{ key: 'AIzaSyD0y-2f3prjfXVnYibVVwWuq4ww2Z7azh8' }}
-//         defaultCenter={defaultProps.center}
-//         defaultZoom={defaultProps.zoom}
-//       >
-//         <AnyReactComponent
-//           lat={32.189731}
-//           lng={34.832381}
-//           text="1 City"
-//         />
-//          <AnyReactComponent
-//           lat={32.089731}
-//           lng={34.831381}
-//           text="2 City"
-//         />
-//          <AnyReactComponent
-//           lat={32.089731}
-//           lng={34.832381}
-//           text="3 City"
-//         />
-//       </GoogleMapReact> 
-//     </div>
-//   );
-// };
-
-
